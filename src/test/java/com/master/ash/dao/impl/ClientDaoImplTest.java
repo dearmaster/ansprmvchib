704807721387fa67620d2817e04de2e61ead4096
dearmaster windows7 <694114533@qq.com>
2016-12-26 00:24:34 +0800

    log4j trace hibernate query parameters

--- src/test/java/com/master/ash/dao/impl/ClientDaoImplTest.java
+++ src/test/java/com/master/ash/dao/impl/ClientDaoImplTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.text.SimpleDateFormat;
+import java.util.HashSet;
 import java.util.Set;
 
 @Transactional
@@ -42,11 +43,26 @@ public class ClientDaoImplTest {
         Assert.assertNotNull(clients);
     }
 
-    @Ignore
+//    @Ignore
     @Test
     public void save() throws Exception {
-        Client client = new Client("Lily", "女", sdf.parse("1995-11-30"), "Beijing", "SAP", "lily@sap.com", "15082383821");
-        clientDao.save(client);
+        Set<Client> set = new HashSet<Client>();
+        set.add(new Client("Lily", "女", sdf.parse("1991-02-21"), "北京", "SAP", "lily@sap.com", "15082301546"));
+        set.add(new Client("Lucy", "女", sdf.parse("1992-03-25"), "上海", "SAP", "Lucy@sap.com", "15082385984"));
+        set.add(new Client("Keven", "女", sdf.parse("1993-01-11"), "武汉", "SAP", "Keven@sap.com", "15082312345"));
+        set.add(new Client("Jacky", "女", sdf.parse("1994-05-22"), "天津", "SAP", "Jacky@sap.com", "15082389547"));
+        set.add(new Client("Poly", "女", sdf.parse("1995-06-16"), "北京", "SAP", "Poly@sap.com", "15082385415"));
+        set.add(new Client("Hatti", "女", sdf.parse("1996-11-26"), "武汉", "SAP", "Hatti@sap.com", "15082396321"));
+        set.add(new Client("Jim", "女", sdf.parse("1997-08-23"), "上海", "SAP", "Jim@sap.com", "15082399999"));
+        set.add(new Client("Lilei", "女", sdf.parse("1996-09-29"), "北京", "SAP", "Lilei@sap.com", "15082344444"));
+        set.add(new Client("Hanmeimei", "女", sdf.parse("1997-10-18"), "天津", "SAP", "Hanmeimei@sap.com", "15082374120"));
+        set.add(new Client("Alex", "女", sdf.parse("1998-11-17"), "武汉", "SAP", "Alex@sap.com", "15082385411"));
+        set.add(new Client("Alan", "女", sdf.parse("1998-05-12"), "上海", "SAP", "Alan@sap.com", "15082311115"));
+        set.add(new Client("Kate", "女", sdf.parse("1999-03-14"), "天津", "SAP", "Kate@sap.com", "15082399884"));
+
+        for(Client client : set) {
+            clientDao.save(client);
+        }
     }
 
 }

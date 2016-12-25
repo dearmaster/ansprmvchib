c2d15c77b5dcd4af7760b3e5da99790c648ba612
dearmaster windows7 <694114533@qq.com>
2016-12-25 20:06:13 +0800

    .

--- src/main/webapp/controller/ClientController.js
+++ src/main/webapp/controller/ClientController.js
@@ -7,20 +7,20 @@ RouteConfigApp.controller('ClientController', function ($scope, $http) {
     }
 
     $scope.deleteClient = function(obj) {
-        var index = $scope.clients.indexOf(obj);
         $http({
             method:'post',
             url:'mvc/client/delete',
             data:obj
         }).success(function (req) {
             console.log(req);
+            var index = $scope.clients.indexOf(obj);
+            $scope.clients.splice(index, 1);
+            console.log('after delete: ' + $scope.clients.length);
+            if($scope.clients.length == 0) {
+                console.log('$scope.hasModels = true');
+                $scope.hasModels = false;
+            }
         });
-        $scope.clients.splice(index, 1);
-        console.log('after delete: ' + $scope.clients.length);
-        if($scope.clients.length == 0) {
-            console.log('$scope.hasModels = true');
-            $scope.hasModels = false;
-        }
     }
 
     $scope.editClient = function(obj) {
